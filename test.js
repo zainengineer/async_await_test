760153a24c9f1d0f728dc8fabb7373436e190d2f
Zain ul abidin <zainengineer@gmail.com>
2017-03-12 13:35:09 +1030

    error: experimenting with error handling async

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const timeout = function (delay) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            // throw ('my test error')
         resolve()
     }, delay)
 })
@@ -8,8 +9,17 @@ const timeout = function (delay) {
 
 async function timer () {
     console.log('timer started');
-    await Promise.resolve(timeout(2000));
+    try{
+        await Promise.resolve(timeout(2000))    ;
+    } catch (e){
+        console.log("Error caught");
+        console.log(e);
+    }
     console.log('timer finished');
 }
-
+process.on('uncaughtException', function(err) {
+    // handle the error safely
+    console.log('caught by universal erorr handler')
+    console.log(err)
+})
 timer();
